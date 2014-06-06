edbe183f83c18fac998e80a6ade6a132832da849
Gregor von Laszewski <laszewski@gmail.com>
2014-06-06 05:39:07 -0400

    imporve  __str__, add to_json

--- reservation/model.py
+++ reservation/model.py
@@ -9,14 +9,31 @@ class Reservation:
     start_time = DateTimeField()
     end_time = DateTimeField()
 
-    def __str__(self):
-        
-        r = "label:", label + "\n" + \
-            + "cm_id:", cm_id + "\n" + \
-            + "summmary:", summmary + "\n" + \
-            + "hosts:", hosts + "\n" + \
-            + "user:", user + "\n" + \
-            + "project:", project + "\n" + \
-            + "start_time:", start_time + "\n" + \
-            + "end_time:", end_time + "\n"
+    _order = [
+        "label",
+        "cm_id",
+        "summmary",
+        "hosts",
+        "user",
+        "project",
+        "start_time",
+        "end_time"
+    ]
 
+    def __str__(self):
+        d = self.to_json()
+        r = ""
+        for key in _order:
+            r = r + key + ": " + d[key]
+        return r
+
+    def to_json(self):
+        d = {"label:", label,
+             "cm_id:", cm_id,
+             "summmary:", summmary,
+             "hosts:", hosts,
+             "user:", user,
+             "project:", project,
+             "start_time:", start_time,
+             "end_time:", end_time}
+        return d
