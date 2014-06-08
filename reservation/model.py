a65f3c013deabf58618451952a89ad9f4361c5fa
Gregor von Laszewski <laszewski@gmail.com>
2014-06-07 23:27:02 -0400

    proper time mamanegemnt

--- reservation/model.py
+++ reservation/model.py
@@ -20,18 +20,18 @@ class Reservation(Document):
 
     cm_id = StringField()
     label = StringField()
-    summmary = StringField()
+    summary = StringField()
     host = StringField()
     user = StringField()
     project = StringField()
     start_time = DateTimeField()
     end_time = DateTimeField()
-    date_modified = DateTimeField(default=datetime.datetime.now)
+    #date_modified = DateTimeField(default=datetime.datetime.now)
 
     _order = [
         "label",
         "cm_id",
-        "summmary",
+        "summary",
         "host",
         "user",
         "project",
@@ -43,14 +43,14 @@ class Reservation(Document):
         d = self.to_json()
         print d
         r = ""
-        for key in self._order:
-            r = r + key + ": " + d[key] + " "
+        #for key in self._order:
+        #    r = r + key + ": " + d[key] + " "
         return r
 
     def to_json(self):
         d = {"label": self.label,
              "cm_id": self.cm_id,
-             "summmary": self.summmary,
+             "summary": self.summary,
              "host": self.host,
              "user": self.user,
              "project": self.project,
